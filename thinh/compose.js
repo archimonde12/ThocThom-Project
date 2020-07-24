88d9f493f31ffe6c9f59cadad7d874af0c66e5d3
thinh1341993 <thinh1341993@gmail.com>
2020-07-24 21:27:37 +0700

    sua form compose

--- thinh/compose.js
+++ thinh/compose.js
@@ -82,6 +82,7 @@ let showForm = document.getElementById('showForm')
 
 showForm.addEventListener('click', function() {
     newIdea.style.display = 'initial';
+    document.getElementById("cke_1_contents").style.height = "250px";
 })
 
 let buttonClose = document.getElementById("buttonClose")
