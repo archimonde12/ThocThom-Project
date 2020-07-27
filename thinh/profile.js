929c216485e3c5331f80a5ae1beb87ca931c400b
thinh1341993 <thinh1341993@gmail.com>
2020-07-27 17:43:45 +0700

    them phan profile

---
+++ thinh/profile.js
@@ -0,0 +1,12 @@
+let Edit = document.getElementById('Edit')
+let myInfor = document.getElementById('myInfor')
+
+
+
+Edit.addEventListener('click', function() {
+    myInfor.style.display = 'flex'
+})
+
+document.getElementById('idea').addEventListener('click', function() {
+    document.getElementById('ideaShow').style.display = 'flex'
+})
